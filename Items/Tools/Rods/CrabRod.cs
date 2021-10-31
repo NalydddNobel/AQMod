b419f8825b75cd68bf4c88984dfb9c0a577dec4c
NalydddNobel <kainnobel6@gmail.com>
2021-10-31 05:25:37 -0700

    this commit should build

--- Items/Tools/Rods/CrabRod.cs
+++ Items/Tools/Rods/CrabRod.cs
@@ -4,7 +4,7 @@ using AQMod.Items.Placeable;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace AQMod.Items.Fishing.Rods
+namespace AQMod.Items.Tools.Rods
 {
     public class CrabRod : ModItem
     {
--- Items/TreasureBags/CrabsonBag.cs
+++ Items/TreasureBags/CrabsonBag.cs
@@ -1,7 +1,7 @@
 ﻿using AQMod.Common.Utilities;
-using AQMod.Items.Fishing.Rods;
 using AQMod.Items.Materials.Energies;
 using AQMod.Items.Tools;
+using AQMod.Items.Tools.Rods;
 using AQMod.Items.Weapons.Magic;
 using AQMod.Items.Weapons.Melee;
 using AQMod.Items.Weapons.Ranged;
