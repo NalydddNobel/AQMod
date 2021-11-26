55892ea03d11874238ff9497e88350c0d7c1ba03
NalydddNobel <kainnobel6@gmail.com>
2021-11-25 22:40:03 -0800

    vrang stuff

--- Items/Weapons/Melee/Vrang.cs
+++ Items/Weapons/Melee/Vrang.cs
@@ -2,6 +2,7 @@
 using AQMod.Common.Utilities;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -10,14 +11,11 @@ namespace AQMod.Items.Weapons.Melee
 {
     public class Vrang : ModItem
     {        
-        private static int _temperature;
-        private float _temp;
-
         public override void SetDefaults()
         {
             item.width = 24;
-            item.height = 24;
-            item.useAnimation = 24;
+            item.height = 20;
+            item.useAnimation = 20;
             item.useTime = 24;
             item.useStyle = ItemUseStyleID.SwingThrow;
             item.melee = true;
@@ -29,53 +27,28 @@ namespace AQMod.Items.Weapons.Melee
             item.channel = true;
             item.noMelee = true;
             item.noUseGraphic = true;
-            item.shootSpeed = 24f;
+            item.autoReuse = true;
+            item.shootSpeed = 22f;
             item.shoot = ModContent.ProjectileType<Projectiles.Melee.Vrang>();
-            item.maxStack = 11;
-            _temperature = 1;
-        }
-
-        public override bool AltFunctionUse(Player player)
-        {
-            return true;
-        }
-
-        public override bool CanUseItem(Player player)
-        {
-            if (!base.CanUseItem(player))
-            {
-                return false;
-            }
-            if (player.altFunctionUse == 2)
-            {
-                _temperature = -1;
-            }
-            else
-            {
-                _temperature = 1;
-            }
-            return true;
         }
 
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
+            var normal = Vector2.Normalize(new Vector2(speedX, speedY));
+            Projectile.NewProjectile(position + normal.RotatedBy(MathHelper.PiOver4 * 3f) * 50f, new Vector2(speedX, speedY), type, damage, knockBack, player.whoAmI);
+            Projectile.NewProjectile(position + normal.RotatedBy(-MathHelper.PiOver4 * 3f) * 50f, new Vector2(speedX, speedY), type, damage, knockBack, player.whoAmI);
             return base.Shoot(player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
-        }
-
-        private void updateTemperature()
-        {
-            _temp = MathHelper.Lerp(_temp, _temperature, 0.01f);
         }
 
         public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
         {
-            updateTemperature();
+            float temp = (float)Math.Sin(Main.GlobalTime);
             Texture2D texture;
-            if (_temp < 0f)
+            if (temp < 0f)
             {
                 texture = ModContent.GetTexture(this.GetPath("_Cold"));
             }
-            else if (_temp > 0f)
+            else if (temp > 0f)
             {
                 texture = ModContent.GetTexture(this.GetPath("_Hot"));
             }
@@ -84,7 +57,7 @@ namespace AQMod.Items.Weapons.Melee
                 return true;
             }
             Main.spriteBatch.Draw(Main.itemTexture[item.type], position, frame, drawColor, 0f, origin, scale, SpriteEffects.None, 0f);
-            Main.spriteBatch.Draw(texture, position, frame, drawColor * _temp.Abs(), 0f, origin, scale, SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(texture, position, frame, drawColor * temp.Abs(), 0f, origin, scale, SpriteEffects.None, 0f);
             return false;
         }
     }
--- Projectiles/Melee/Vrang.cs
+++ Projectiles/Melee/Vrang.cs
@@ -1,6 +1,7 @@
-﻿using AQMod.Common;
+﻿using AQMod.Common.Utilities;
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace AQMod.Projectiles.Melee
@@ -11,10 +12,14 @@ namespace AQMod.Projectiles.Melee
         {
             projectile.width = 32;
             projectile.height = 32;
+            projectile.aiStyle = -1;
             projectile.friendly = true;
             projectile.scale = 0.9f;
             projectile.penetrate = -1;
             projectile.melee = true;
+            projectile.alpha = 250;
+            projectile.timeLeft = 60;
+            projectile.penetrate = 3;
         }
 
         public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough)
@@ -26,19 +31,84 @@ namespace AQMod.Projectiles.Melee
 
         public override void AI()
         {
-            if ((int)projectile.ai[0] == 0)
+            if ((int)projectile.ai[0] == -1)
             {
-                projectile.velocity = Vector2.Normalize(projectile.velocity);
+                projectile.alpha = 0;
+                projectile.friendly = false;
+                projectile.tileCollide = false;
+                projectile.velocity.X *= 0.99f;
+                if (projectile.velocity.Y < -4f)
+                {
+                    projectile.velocity.Y *= 0.9f;
+                }
+                if (projectile.velocity.Y < 16f)
+                    projectile.velocity.Y += 0.3f;
+                projectile.rotation += projectile.velocity.Length() * 0.02f;
+                return;
             }
             projectile.ai[0]++;
-            if (projectile.ai[0] > 30f * Main.player[projectile.owner].meleeSpeed)
+            if (projectile.ai[0] > 10f * Main.player[projectile.owner].meleeSpeed)
             {
-                projectile.velocity += projectile.velocity * 0.001f;
+                if (projectile.velocity.Length() < 22f / Main.player[projectile.owner].meleeSpeed)
+                    projectile.velocity += projectile.velocity * 0.08f;
+
+                projectile.localAI[1] += 0.005f;
+                projectile.rotation += 0.3f + projectile.localAI[1];
             }
             else
             {
-                projectile.velocity += Vector2.Normalize(projectile.velocity) * 0.0125f;
+                if (projectile.velocity.Length() > 1f)
+                    projectile.velocity *= 0.78f;
+                projectile.rotation += 0.3f;
             }
+            if (projectile.alpha > 0)
+            {
+                projectile.alpha -= 25;
+                if (projectile.alpha < 0)
+                {
+                    projectile.alpha = 0;
+                }
+            }
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Main.PlaySound(SoundID.Dig, (int)projectile.position.X, (int)projectile.position.Y);
+            if (projectile.alpha > 100)
+            {
+                return base.OnTileCollide(oldVelocity);
+            }
+            bool hitEffect = false;
+            if (oldVelocity.X != projectile.velocity.X)
+            {
+                if (oldVelocity.X.Abs() > 4f)
+                    hitEffect = true;
+                projectile.position.X += projectile.velocity.X;
+                if (oldVelocity.X > oldVelocity.Y)
+                    projectile.velocity.X = -oldVelocity.X;
+                else
+                    projectile.velocity.X = -oldVelocity.X * 0.3f;
+            }
+            if (oldVelocity.Y != projectile.velocity.Y)
+            {
+                if (oldVelocity.Y.Abs() > 4f)
+                    hitEffect = true;
+                projectile.position.Y += projectile.velocity.Y;
+                if (oldVelocity.Y > oldVelocity.X)
+                    projectile.velocity.Y = -oldVelocity.Y;
+                else
+                    projectile.velocity.Y = -oldVelocity.Y * 0.3f;
+            }
+            if (hitEffect)
+            {
+                projectile.ai[0] = -1f;
+                projectile.timeLeft = 120;
+                projectile.netUpdate = true;
+                projectile.velocity = projectile.velocity.RotatedBy(Main.rand.NextFloat(-0.25f, 0.25f));
+                Collision.HitTiles(projectile.position, oldVelocity, projectile.width, projectile.height);
+                return false;
+            }
+            return base.OnTileCollide(oldVelocity);
         }
     }
 }
