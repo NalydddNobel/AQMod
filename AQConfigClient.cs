d34a12f40324ee71dcb14d8299ed4dd95bea6847
NalydddNobel <kainnobel6@gmail.com>
2021-12-24 02:35:30 -0800

    config colors and lang

--- AQConfigClient.cs
+++ AQConfigClient.cs
@@ -18,6 +18,7 @@ namespace AQMod
 
         [Header(AQText.ConfigHeaderKey + "Visuals")]
 
+        [BackgroundColor(140, 29, 47, 180)]
         [Label(AQText.ConfigValueKey + "EffectQuality")]
         [DefaultValue(1f)]
         [Range(0.1f, 2f)]
@@ -27,6 +28,7 @@ namespace AQMod
         /// </summary>
         public static float c_EffectQuality { get; private set; }
 
+        [BackgroundColor(140, 29, 47, 180)]
         [Label(AQText.ConfigValueKey + "EffectIntensity")]
         [DefaultValue(1f)]
         [Range(0.1f, 1f)]
@@ -36,27 +38,64 @@ namespace AQMod
         /// </summary>
         public static float c_EffectIntensity { get; private set; }
 
-        [Label(AQText.ConfigValueKey + "Effect3D")]
-        [Tooltip(AQText.ConfigValueKey + "Effect3DTooltip")]
-        [DefaultValue(1f)]
-        [Range(0f, 1.5f)]
-        public float Effect3D { get; set; }
-        public static float c_Effect3D { get; private set; }
-
+        [BackgroundColor(140, 29, 47, 180)]
         [Label("$" + AQText.Key + "ClientConfig.Screenshakes")]
         [DefaultValue(true)]
         public bool Screenshakes { get; set; }
         public static bool c_Screenshakes { get; private set; }
 
+        [BackgroundColor(140, 29, 47, 180)]
         [Label("$" + AQText.Key + "ClientConfig.TonsofScreenShakes")]
         [DefaultValue(false)]
         public bool TonsofScreenShakes { get; set; }
         public static bool c_TonsofScreenShakes { get; private set; }
 
-        [Label(AQText.ConfigValueKey + "BackgroundStarites")]
+        [Header("$" + AQText.Key + "ClientConfig.Header.Worldgen")]
+
+        [BackgroundColor(166, 166, 12, 180)]
+        [Label("$" + AQText.Key + "ClientConfig.OverrideVanillaChestLoot")]
+        [Tooltip("$" + AQText.Key + "ClientConfig.OverrideVanillaChestLootTooltip")]
+        [DefaultValue(true)]
+        public bool OverrideVanillaChestLoot { get; private set; }
+        public static bool c_OverrideVanillaChestLoot { get; private set; }
+
+        [Header("$Mods.AQMod.ClientConfig.Header.Starite")]
+
+        [BackgroundColor(13, 166, 231, 180)]
+        [Label("$Mods.AQMod.ClientConfig.Effect3D")]
+        [Tooltip("$Mods.AQMod.ClientConfig.Effect3DTooltip")]
+        [DefaultValue(1f)]
+        [Range(0f, 1.5f)]
+        public float Effect3D { get; set; }
+        public static float c_Effect3D { get; private set; }
+
+        [BackgroundColor(13, 166, 231, 180)]
+        [Label("$Mods.AQMod.ClientConfig.BackgroundStarites")]
+        [Tooltip("$Mods.AQMod.ClientConfig.BackgroundStaritesTooltip")]
         [DefaultValue(true)]
         public bool BackgroundStarites { get; set; }
         public static bool c_BackgroundStarites { get; private set; }
+
+        [BackgroundColor(13, 166, 231, 180)]
+        [Label("$Mods.AQMod.ClientConfig.StariteProjColor")]
+        [Tooltip("$Mods.AQMod.ClientConfig.StariteProjColorTooltip")]
+        [DefaultValue(typeof(Color), "200, 10, 255, 0"), ColorNoAlpha]
+        public Color StariteProjColor { get; set; }
+
+        [BackgroundColor(13, 166, 231, 180)]
+        [Label("$Mods.AQMod.ClientConfig.StariteAuraColor")]
+        [Tooltip("$Mods.AQMod.ClientConfig.StariteAuraColorTooltip")]
+        [DefaultValue(typeof(Color), "100, 100, 255, 0"), ColorNoAlpha]
+        public Color StariteAuraColor { get; set; }
+
+        [BackgroundColor(13, 166, 231, 180)]
+        [Label("$Mods.AQMod.ClientConfig.StariteBackgroundLight")]
+        [Tooltip("$Mods.AQMod.ClientConfig.StariteBackgroundLightTooltip")]
+        [DefaultValue(1f)]
+        [Range(0f, 1f)]
+        public float StariteBackgroundLight { get; set; }
+
+        [Header("$Mods.AQMod.ClientConfig.Header.Xmas")]
 
         [BackgroundColor(75, 80, 100, 180)]
         [Label("$Mods.AQMod.ClientConfig.XmasBackground")]
@@ -72,41 +111,24 @@ namespace AQMod
         [ReloadRequired()]
         public bool XmasProgressMeterOverride { get; set; }
 
-        [Label(AQText.ConfigValueKey + "MapBlipColor")]
+        [Header("$Mods.AQMod.ClientConfig.Header.UI")]
+
+        [BackgroundColor(12, 12, 190, 180)]
+        [Label("$Mods.AQMod.ClientConfig.MapBlipColor")]
+        [Tooltip("$Mods.AQMod.ClientConfig.MapBlipColorTooltip")]
         [DefaultValue(typeof(Color), "200, 60, 145, 255"), ColorNoAlpha]
         public Color MapBlipColor { get; set; }
 
-        [Label(AQText.ConfigValueKey + "StariteProjColor")]
-        [DefaultValue(typeof(Color), "200, 10, 255, 0"), ColorNoAlpha]
-        public Color StariteProjColor { get; set; }
-
-        [Label(AQText.ConfigValueKey + "StariteAuraColor")]
-        [Tooltip(AQText.ConfigValueKey + "StariteAuraColorTooltip")]
-        [DefaultValue(typeof(Color), "100, 100, 255, 0"), ColorNoAlpha]
-        public Color StariteAuraColor { get; set; }
-
-        [Label(AQText.ConfigValueKey + "StariteBackgroundLight")]
-        [Tooltip(AQText.ConfigValueKey + "StariteBackgroundLightTooltip")]
-        [DefaultValue(1f)]
-        [Range(0f, 1f)]
-        public float StariteBackgroundLight { get; set; }
-
-        [Header("$" + AQText.Key + "ClientConfig.Header.Worldgen")]
-
-        [Label("$" + AQText.Key + "ClientConfig.OverrideVanillaChestLoot")]
-        [Tooltip("$" + AQText.Key + "ClientConfig.OverrideVanillaChestLootTooltip")]
-        [DefaultValue(true)]
-        public bool OverrideVanillaChestLoot;
-        public static bool c_OverrideVanillaChestLoot { get; private set; }
-
         [Header(AQText.ConfigHeaderKey + "Misc")]
 
+        [BackgroundColor(150, 12, 166, 180)]
         [Label(AQText.ConfigValueKey + "CosmicEnergyAlt")]
         [DefaultValue(false)]
         public bool CosmicEnergyAlt { get; set; }
         public static bool c_CosmicEnergyAlt { get; private set; }
 
         [Label(AQText.ConfigValueKey + "ShowCompletedAnglerQuestsCount")]
+        [BackgroundColor(12, 12, 190, 180)]
         [DefaultValue(true)]
         public bool ShowCompletedQuestsCount { get; set; }
 
