7171f54b87c8acc3e18ef58d682711fae828c46d
NalydddNobel <kainnobel6@gmail.com>
2022-06-13 14:39:15 -0700

    sentry potion and zh-Hans fix

---
+++ Buffs/SentryBuff.cs
@@ -0,0 +1,13 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace Aequus.Buffs
+{
+    public class SentryBuff : ModBuff
+    {
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.maxTurrets++;
+        }
+    }
+}
---
+++ Items/Consumables/Potions/SentryPotion.cs
@@ -0,0 +1,32 @@
+﻿using Aequus.Buffs;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Aequus.Items.Consumables.Potions
+{
+    public class SentryPotion : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            ItemID.Sets.DrinkParticleColors[Type] = new Color[] { new Color(208, 101, 32, 0), new Color(241, 216, 109, 0), new Color(138, 76, 31, 0), };
+            this.SetResearch(20);
+        }
+
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.SummoningPotion);
+            Item.buffType = ModContent.BuffType<SentryBuff>();
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.BottledWater)
+                .AddIngredient(ItemID.FrostMinnow)
+                .AddIngredient(ItemID.Shiverthorn)
+                .AddTile(TileID.Bottles)
+                .Register((r) => r.SortBeforeFirstRecipesOf(ItemID.SummoningPotion));
+        }
+    }
+}
