4690ea9e90c267b274b313002c03c620bba25eb4
NalydddNobel <kainnobel6@gmail.com>
2023-02-15 13:35:10 -0800

    removed 1 line of debug lul

--- Aequus.cs
+++ Aequus.cs
@@ -161,7 +161,6 @@ namespace Aequus
 
         public static bool AllModesGetExpertExclusives()
         {
-            return true;
             return CalamityMod.Instance != null;
         }
     }
