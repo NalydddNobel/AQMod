181c43e3ac2b7580b1e23dc9609d3cadaaea3302
NalydddNobel <kainnobel6@gmail.com>
2023-03-25 14:47:46 -0700

    Improve Possessed Shard

--- Items/Materials/PossessedShard.cs
+++ Items/Materials/PossessedShard.cs
@@ -11,6 +11,7 @@ using Terraria.ModLoader;
 namespace Aequus.Items.Materials {
     public class PossessedShard : ModItem {
 
+        private int _shake;
         private int _soundDelay;
         private int _applyCollision;
         private Vector2 fleeVelocity;
@@ -36,6 +37,7 @@ namespace Aequus.Items.Materials {
             fleeVelocity = Vector2.Zero;
             _applyCollision = 16;
             _soundDelay = 0;
+            _shake = 0;
             Item.timeSinceItemSpawned = 0;
         }
 
@@ -60,24 +62,34 @@ namespace Aequus.Items.Materials {
                     fleeVelocity *= 7f;
                 }
 
+
+                int dustSizeAdd = 10;
+                var dustPosition = Item.position - Vector2.Normalize(fleeVelocity) * (Item.width + 4) - new Vector2(dustSizeAdd / 2f);
                 for (int i = 0; i < 8; i++) {
-                    var d = Dust.NewDustDirect(Item.position, Item.width, Item.height, DustID.PurpleTorch, Scale: Main.rand.NextFloat(2f));
+                    var d = Dust.NewDustDirect(dustPosition, Item.width + dustSizeAdd, Item.height + dustSizeAdd, DustID.PurpleTorch, Scale: Main.rand.NextFloat(2f));
                     d.velocity = Vector2.Normalize(fleeVelocity) * Main.rand.NextFloat(3f);
                     d.noGravity = true;
                     d.fadeIn = d.scale + 0.5f;
                 }
 
                 if (_soundDelay <= 0) {
-                    SoundEngine.PlaySound(AequusSounds.shardHit with { Volume = 0.8f, PitchVariance = 0.1f, }, Item.position);
+                    SoundEngine.PlaySound(AequusSounds.shardHit with { Volume = 0.8f, Pitch = -0.3f, PitchVariance = 0.1f, }, Item.position);
                 }
-                _soundDelay = 8;
                 if (Main.netMode == NetmodeID.Server) {
                     NetMessage.SendData(MessageID.SyncItem, number: Item.whoAmI);
                 }
+
+                _shake = 8;
+                _applyCollision = -8;
+                _soundDelay = 8;
             }
         }
 
         public override void Update(ref float gravity, ref float maxFallSpeed) {
+
+            if (_shake > 0 && Item.timeSinceItemSpawned % 3 == 0) {
+                _shake--;
+            }
 
             if (Item.timeSinceItemSpawned < 50) {
                 return;
@@ -96,31 +108,36 @@ namespace Aequus.Items.Materials {
             if (!Helper.CheckForSolidGroundBelow(Item.Center.ToTileCoordinates(), 12, out var _)) {
                 Item.velocity.Y += 0.4f;
             }
+            if (!Helper.CheckForSolidRoofAbove(Item.Center.ToTileCoordinates(), 4, out var _)) {
+                Item.velocity.Y -= 0.14f;
+            }
 
             float moveSpeed = Main.expertMode ? 0.4f : 0.2f;
             float viewDistance = (fleeVelocity.X != 0f || fleeVelocity.Y != 0f) ? 300f : 150f;
             bool fleeing = false;
-            for (int i = 0; i < Main.maxPlayers; i++) {
-                if (Main.player[i].active && Item.Distance(Main.player[i].Center) < viewDistance) {
+            if (_applyCollision >= 0) {
+                for (int i = 0; i < Main.maxPlayers; i++) {
+                    if (Main.player[i].active && Item.Distance(Main.player[i].Center) < viewDistance) {
 
-                    var dir = Item.DirectionFrom(Main.player[i].Center);
-                    if (fleeVelocity == Vector2.Zero) {
-                        fleeVelocity = dir * 10f;
-                        SoundEngine.PlaySound(AequusSounds.dash);
-                        if (Main.netMode == NetmodeID.Server) {
-                            NetMessage.SendData(MessageID.SyncItem, number: Item.whoAmI);
+                        var dir = Item.DirectionFrom(Main.player[i].Center);
+                        if (fleeVelocity == Vector2.Zero) {
+                            fleeVelocity = dir * 10f;
+                            SoundEngine.PlaySound(AequusSounds.dash);
+                            if (Main.netMode == NetmodeID.Server) {
+                                NetMessage.SendData(MessageID.SyncItem, number: Item.whoAmI);
+                            }
+
+                            for (int j = 0; j < 8; j++) {
+                                var d = Dust.NewDustDirect(Item.position, Item.width, Item.height, DustID.PurpleTorch, Scale: Main.rand.NextFloat(2f));
+                                d.velocity = Vector2.Normalize(fleeVelocity) * Main.rand.NextFloat(3f);
+                                d.noGravity = true;
+                                d.fadeIn = d.scale + 0.5f;
+                            }
                         }
 
-                        for (int j = 0; j < 8; j++) {
-                            var d = Dust.NewDustDirect(Item.position, Item.width, Item.height, DustID.PurpleTorch, Scale: Main.rand.NextFloat(2f));
-                            d.velocity = Vector2.Normalize(fleeVelocity) * Main.rand.NextFloat(3f);
-                            d.noGravity = true;
-                            d.fadeIn = d.scale + 0.5f;
-                        }
+                        fleeVelocity += dir * moveSpeed;
+                        fleeing = true;
                     }
-
-                    fleeVelocity += dir * moveSpeed;
-                    fleeing = true;
                 }
             }
 
@@ -136,6 +153,9 @@ namespace Aequus.Items.Materials {
             }
             else if (_applyCollision > 0) {
                 _applyCollision--;
+            }
+            else if (_applyCollision < 0) {
+                _applyCollision++;
             }
 
             if (_applyCollision < 16) {
@@ -169,6 +189,10 @@ namespace Aequus.Items.Materials {
             float progress = MathF.Pow(1f - Main.GlobalTimeWrappedHourly % 1f, 2f);
             var glowColor = Color.BlueViolet with { A = 0 };
             var vector = Helper.CircularVector(4, progress * MathHelper.PiOver2);
+
+            if (_shake > 0) {
+                drawCoordinates += new Vector2(Main.rand.Next(-_shake, _shake), Main.rand.Next(-_shake, _shake)) / 1.5f;
+            }
 
             if (progress > 0.5f) {
 
