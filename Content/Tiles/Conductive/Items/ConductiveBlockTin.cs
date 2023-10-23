b07342c64a3ca63973091081994e1b3fcedf8af4
NalydddNobel <kainnobel6@gmail.com>
2023-10-23 07:08:32 -0700

    marked conductive blocks as WIP

--- Content/Tiles/Conductive/Items/ConductiveBlockTin.cs
+++ Content/Tiles/Conductive/Items/ConductiveBlockTin.cs
@@ -1,8 +1,10 @@
-﻿using Terraria.ID;
+﻿using Aequus.Core;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Aequus.Content.Tiles.Conductive.Items;
 
+[WorkInProgress]
 public class ConductiveBlockTin : ConductiveBlock {
     public override int BarItem => ItemID.TinBar;
     public override int TileId => ModContent.TileType<ConductiveBlockTileTin>();
