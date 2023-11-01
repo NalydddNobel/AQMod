51a1666e01312145ad3ffec94f5894e7ccc539b0
NalydddNobel <kainnobel6@gmail.com>
2023-11-01 11:20:56 -0700

    jellyfish brains obtained (they are very dumb)

---
+++ Content/Enemies/PollutedOcean/BlackJellyfish/BlackJellyfish.cs
@@ -0,0 +1,14 @@
+﻿using Aequus.Common.NPCs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aequus.Content.Enemies.PollutedOcean.BlackJellyfish;
+
+public class BlackJellyfish : AIJellyfish {
+    public override void SetDefaults() {
+        NPC.lifeMax = 10;
+    }
+}
