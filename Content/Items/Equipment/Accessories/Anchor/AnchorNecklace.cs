7a8d41b55f4ad5e27e2868ab0b9afd2c5c60eb65
NalydddNobel <kainnobel6@gmail.com>
2023-11-01 17:40:31 -0700

    yeag

--- Content/Items/Equipment/Accessories/Anchor/AnchorNecklace.cs
+++ Content/Items/Equipment/Accessories/Anchor/AnchorNecklace.cs
@@ -1,9 +1,11 @@
 ﻿using Aequus.Common.Items;
+using Aequus.Core;
 using Terraria;
 using Terraria.ModLoader;
 
 namespace Aequus.Content.Items.Equipment.Accessories.Anchor;
 
+[WorkInProgress]
 public class AnchorNecklace : ModItem {
     public override void SetDefaults() {
         Item.DefaultToAccessory();
