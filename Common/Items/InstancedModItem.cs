6220efd0dbe2ed98342fd1dfde6553998c8026b1
NalydddNobel <kainnobel6@gmail.com>
2023-11-23 19:37:00 -0800

    items directory fallback

--- Common/Items/InstancedModItem.cs
+++ Common/Items/InstancedModItem.cs
@@ -1,4 +1,7 @@
-﻿using Terraria.ModLoader;
+﻿using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Text;
+using Terraria;
+using Terraria.ModLoader;
 
 namespace Aequus.Common.Items;
 
@@ -12,8 +15,27 @@ internal abstract class InstancedModItem : ModItem {
 
     protected override bool CloneNewInstances => true;
 
+    private void TryAlternativeTexturePaths(ref string texture) {
+        if (ModContent.HasAsset(texture)) {
+            return;
+        }
+
+        int i = texture.LastIndexOf('/');
+        string name = texture[(i + 1)..];
+        string path = texture[..i];
+
+        string itemsFolder = path + "/Items/" + name;
+        if (ModContent.HasAsset(itemsFolder)) {
+            texture = itemsFolder;
+            return;
+        }
+    }
+
     public InstancedModItem(string name, string texture) {
         _name = name;
         _texture = texture;
+        if (!Main.dedServ) {
+            TryAlternativeTexturePaths(ref _texture);
+        }
     }
 }
--- Temp/SourceGenerators/SourceGenerators.TextureSourceGenerator/AequusTextures.cs
+++ Temp/SourceGenerators/SourceGenerators.TextureSourceGenerator/AequusTextures.cs
@@ -415,14 +415,14 @@ public partial class AequusTextures {
     public static readonly RequestCache<Texture2D> ConductiveBlockTinItem = new("Aequus/Content/Tiles/Conductive/ConductiveBlockTinItem");
     /// <summary>Full Path: Content/Tiles/CrabPots/CrabPot</summary>
     public static readonly RequestCache<Texture2D> CrabPot = new("Aequus/Content/Tiles/CrabPots/CrabPot");
-    /// <summary>Full Path: Content/Tiles/CrabPots/CrabPotCopperItem</summary>
-    public static readonly RequestCache<Texture2D> CrabPotCopperItem = new("Aequus/Content/Tiles/CrabPots/CrabPotCopperItem");
-    /// <summary>Full Path: Content/Tiles/CrabPots/CrabPotTinItem</summary>
-    public static readonly RequestCache<Texture2D> CrabPotTinItem = new("Aequus/Content/Tiles/CrabPots/CrabPotTinItem");
     /// <summary>Full Path: Content/Tiles/CrabPots/CrabPot_Back</summary>
     public static readonly RequestCache<Texture2D> CrabPot_Back = new("Aequus/Content/Tiles/CrabPots/CrabPot_Back");
     /// <summary>Full Path: Content/Tiles/CrabPots/CrabPot_Highlight</summary>
     public static readonly RequestCache<Texture2D> CrabPot_Highlight = new("Aequus/Content/Tiles/CrabPots/CrabPot_Highlight");
+    /// <summary>Full Path: Content/Tiles/CrabPots/Items/CrabPotCopperItem</summary>
+    public static readonly RequestCache<Texture2D> CrabPotCopperItem = new("Aequus/Content/Tiles/CrabPots/Items/CrabPotCopperItem");
+    /// <summary>Full Path: Content/Tiles/CrabPots/Items/CrabPotTinItem</summary>
+    public static readonly RequestCache<Texture2D> CrabPotTinItem = new("Aequus/Content/Tiles/CrabPots/Items/CrabPotTinItem");
     /// <summary>Full Path: Content/Tiles/CraftingStations/TrashCompactor/TrashCompactor</summary>
     public static readonly RequestCache<Texture2D> TrashCompactor = new("Aequus/Content/Tiles/CraftingStations/TrashCompactor/TrashCompactor");
     /// <summary>Full Path: Content/Tiles/CraftingStations/TrashCompactor/TrashCompactorItem</summary>
