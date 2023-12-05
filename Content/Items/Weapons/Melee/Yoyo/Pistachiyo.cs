970218ae31c26ff7544b739120af12d73428ee55
NalydddNobel <kainnobel6@gmail.com>
2023-12-05 04:13:10 -0800

    gleh

---
+++ Content/Items/Weapons/Melee/Yoyo/Pistachiyo.cs
@@ -0,0 +1,58 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace Aequus.Content.Items.Weapons.Melee.Yoyo.Pistachiyo;
+
+public class Pistachiyo : ModItem {
+    public static int HealAmount = 5;
+    public static int MaximumVines = 8;
+    public static int MaximumShells = 2;
+
+    public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(MaximumVines, HealAmount);
+
+    public override void SetStaticDefaults() {
+        ItemID.Sets.Yoyo[Item.type] = true;
+        ItemID.Sets.GamepadExtraRange[Item.type] = 18;
+        ItemID.Sets.GamepadSmartQuickReach[Item.type] = true;
+    }
+
+    public override void SetDefaults() {
+        Item.width = 24;
+        Item.height = 24;
+
+        Item.useStyle = ItemUseStyleID.Shoot;
+        Item.useTime = 25;
+        Item.useAnimation = 25;
+        Item.noMelee = true;
+        Item.noUseGraphic = true;
+        Item.UseSound = SoundID.Item1;
+
+        Item.damage = 58;
+        Item.DamageType = DamageClass.MeleeNoSpeed;
+        Item.knockBack = 2.5f;
+        Item.channel = true;
+        Item.rare = ItemRarityID.Yellow;
+        Item.value = Item.buyPrice(gold: 4);
+
+        Item.shoot = ModContent.ProjectileType<PistachiyoProj>();
+        Item.shootSpeed = 16f;
+    }
+
+    public override void HoldItem(Player player) {
+        if (player.stringColor == 0) {
+            player.stringColor = 4; // Makes the string green
+        }
+    }
+
+    public override void AddRecipes() {
+        /*
+         * CreateRecipe()
+         *    .AddIngredient<CrackinClam>()
+         *    .AddIngredient<OrganicEnergy>()
+         *    .AddTile(TileID.MythrilAnvil)
+         *    .Register();
+         */
+    }
+}
--- Temp/SourceGenerators/SourceGenerators.TextureSourceGenerator/AequusTextures.cs
+++ Temp/SourceGenerators/SourceGenerators.TextureSourceGenerator/AequusTextures.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 
 namespace Aequus;
 
-/// <summary>(Total Textures: 243)</summary>
+/// <summary>(Total Textures: 246)</summary>
 [CompilerGenerated]
 public partial class AequusTextures {            
         /// <summary>Full Path: Assets/Textures/Achievements</summary>
@@ -385,6 +385,12 @@ public partial class AequusTextures {
     public static readonly RequestCache<Texture2D> Slice = new("Aequus/Content/Items/Weapons/Melee/Swords/Slice/Slice");
     /// <summary>Full Path: Content/Items/Weapons/Melee/Swords/Slice/SliceBulletProj</summary>
     public static readonly RequestCache<Texture2D> SliceBulletProj = new("Aequus/Content/Items/Weapons/Melee/Swords/Slice/SliceBulletProj");
+    /// <summary>Full Path: Content/Items/Weapons/Melee/Yoyo/Pistachiyo</summary>
+    public static readonly RequestCache<Texture2D> Pistachiyo = new("Aequus/Content/Items/Weapons/Melee/Yoyo/Pistachiyo");
+    /// <summary>Full Path: Content/Items/Weapons/Melee/Yoyo/PistachiyoProj</summary>
+    public static readonly RequestCache<Texture2D> PistachiyoProj = new("Aequus/Content/Items/Weapons/Melee/Yoyo/PistachiyoProj");
+    /// <summary>Full Path: Content/Items/Weapons/Melee/Yoyo/PistachiyoProj_Shells</summary>
+    public static readonly RequestCache<Texture2D> PistachiyoProj_Shells = new("Aequus/Content/Items/Weapons/Melee/Yoyo/PistachiyoProj_Shells");
     /// <summary>Full Path: Content/Items/Weapons/Ranged/Bows/SkyHunterCrossbow/Chain</summary>
     public static readonly RequestCache<Texture2D> Chain = new("Aequus/Content/Items/Weapons/Ranged/Bows/SkyHunterCrossbow/Chain");
     /// <summary>Full Path: Content/Items/Weapons/Ranged/Bows/SkyHunterCrossbow/SkyHunterCrossbow</summary>
