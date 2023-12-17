fd2acc9cdfb5d7cb6d6dbcf7168375029c38c8f9
NalydddNobel <kainnobel6@gmail.com>
2023-12-17 09:28:02 -0800

    random calamity stats support

--- Content/CrossMod/CalamityMod.cs
+++ Content/CrossMod/CalamityMod.cs
@@ -1,11 +1,17 @@
-﻿using Aequus.Core.CrossMod;
+﻿using Aequus.Common.Players.Stats;
+using Aequus.Core.CrossMod;
 using System;
-using System.Collections.Generic;
-using Terraria;
+using Terraria.Localization;
 
 namespace Aequus.Content.CrossMod;
 
 internal class CalamityMod : ModSupport<CalamityMod> {
     public override void PostSetupContent() {
+        StatCompareLoader.Register(new ComparePercent((p) => TryCall(out float value, "GetRogueVelocity", p) ? value : 0f, Affix("RogueVelocity")));
+        StatCompareLoader.Register(new CompareFloat((p) => TryCall(out float value, "GetMaxStealth", p) ? value * 100f : 0f, Affix("MaxStealth")));
+
+        static LocalizedText Affix(string key, Func<string> createDefault = null) {
+            return StatCompareLoader.Affix("CalamityMod." + key, createDefault);
+        }
     }
 }
