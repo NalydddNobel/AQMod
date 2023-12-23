52d3e8fa1796c9f8d2be9b254e879c992028d94b
NalydddNobel <kainnobel6@gmail.com>
2023-12-23 01:20:13 -0800

    trash staff re-use delay increase, damage increase compensation

--- Content/Weapons/Magic/TrashStaff/TrashStaff.cs
+++ Content/Weapons/Magic/TrashStaff/TrashStaff.cs
@@ -10,12 +10,12 @@ public class TrashStaff : ModItem {
 
     public override void SetDefaults() {
         Item.DefaultToMagicWeapon(ModContent.ProjectileType<TrashStaffProj>(), 4, 14f, hasAutoReuse: true);
-        Item.SetWeaponValues(13, 1f, bonusCritChance: 8);
+        Item.SetWeaponValues(17, 1f, bonusCritChance: 8);
         Item.rare = ItemCommons.Rarity.CrabsonLoot;
         Item.value = ItemCommons.Price.CrabsonLoot;
         Item.mana = 10;
         Item.useAnimation *= 5;
-        Item.reuseDelay = 15;
+        Item.reuseDelay = 25;
         Item.UseSound = SoundID.Item8;
     }
 
