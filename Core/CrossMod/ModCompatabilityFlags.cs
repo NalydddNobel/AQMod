2537af510c31d5649370edabe6a7f1eb02553aff
NalydddNobel <kainnobel6@gmail.com>
2024-01-17 07:03:24 -0800

    omg i love the Automaton from magic storage

---
+++ Core/CrossMod/ModCompatabilityFlags.cs
@@ -0,0 +1,12 @@
+﻿namespace Aequus.Core.CrossMod;
+
+internal class ModCompatabilityFlags : ILoadable {
+    public static bool RemoveExpertExclusivity { get; set; }
+
+    public void Load(Mod mod) {
+    }
+
+    public void Unload() {
+        RemoveExpertExclusivity = false;
+    }
+}
