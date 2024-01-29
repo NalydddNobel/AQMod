d2a0ffd0b4a27bac282bdbe05a0ba3fa874d58de
NalydddNobel <kainnobel6@gmail.com>
2024-01-29 14:09:49 -0800

    port gen step code from polluted ocean branch

--- Common/WorldGeneration/AequusGenStep.cs
+++ Common/WorldGeneration/AequusGenStep.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using Aequus.Core.Initialization;
+using System;
+using System.Collections.Generic;
 using Terraria.GameContent.Generation;
 using Terraria.IO;
 using Terraria.Utilities;
@@ -6,13 +8,33 @@ using Terraria.WorldBuilding;
 
 namespace Aequus.Common.WorldGeneration;
 
-public abstract class AequusGenStep : ModType {
+public abstract class AequusGenStep : ModType, ILocalizedModType, IPostSetupContent {
     public abstract string InsertAfter { get; }
+
+    public double Weight { get; private set; }
 
     public static UnifiedRandom Random => WorldGen.genRand;
 
-    protected sealed override void Register() {
-        WorldGenSystem.GenerationSteps.Add(this);
+    protected virtual double GenWeight { get; }
+
+    public string LocalizationCategory => "GenStep";
+
+    protected string name;
+    public override string Name => name;
+
+    private string _genMessage;
+
+    public AequusGenStep() {
+        name = base.Name.Replace("Generator", "").Replace("Step", "");
+    }
+
+    public abstract void Apply(GenerationProgress progress, GameConfiguration config);
+
+    public virtual void PostSetupContent() {
+    }
+
+    public virtual GenPass CreateGenerationPass() {
+        return new PassLegacy(Name, Apply, Weight);
     }
 
     public virtual void EmergencyOnStepNotFound(List<GenPass> tasks) {
@@ -23,9 +45,82 @@ public abstract class AequusGenStep : ModType {
         tasks.Insert(index, CreateGenerationPass());
     }
 
-    public abstract void Apply(GenerationProgress progress, GameConfiguration config);
+    public virtual void Reset() {
+    }
 
-    public virtual GenPass CreateGenerationPass() {
-        return new PassLegacy(Name, Apply);
+    protected sealed override void Register() {
+        Weight = GenWeight;
+        WorldGenSystem.GenerationSteps.Add(this);
+    }
+
+    public void PostSetupContent(Aequus aequus) {
+        this.GetLocalization("DisplayMessage");
+        PostSetupContent();
+    }
+
+    /// <summary>
+    /// Safely sets the progress message.
+    /// </summary>
+    /// <param name="progress"></param>
+    /// <param name="keySuffix"></param>
+    protected void SetMessage(GenerationProgress progress, object keySuffix = null) {
+        if (progress != null) {
+            _genMessage = this.GetLocalizedValue($"DisplayMessage{(keySuffix == null ? "" : "." + keySuffix)}");
+            progress.Message = _genMessage;
+        }
+    }
+
+    protected void SetSubMessage(GenerationProgress progress, string key = null) {
+        if (progress != null) {
+            progress.Message = _genMessage + this.GetLocalizedValue(key);
+        }
+    }
+
+    /// <summary>Determines the progress of an ij loop in a rectangle. Using 0-<see cref="Main.maxTilesX"/> and 0-<see cref="Main.maxTilesY"/> as the edges.</summary>
+    /// <param name="i"></param>
+    /// <param name="j"></param>
+    protected static double RectangleProgress(int i, int j) {
+        return RectangleProgress(i, j, 0, Main.maxTilesX, 0, Main.maxTilesY);
+    }
+    /// <summary>Determines the progress of an ij loop in a rectangle. Using 0-<see cref="Main.maxTilesY"/> as the vertical edges.</summary>
+    /// <param name="i"></param>
+    /// <param name="j"></param>
+    /// <param name="leftSide">X coordinate of the Left edge of the rectangle.</param>
+    /// <param name="rightSide">X coordinate of the Right edge of the rectnagle.</param>
+    protected static double RectangleProgress(int i, int j, int leftSide, int rightSide) {
+        return RectangleProgress(i, j, leftSide, rightSide, 0, Main.maxTilesY);
+    }
+    /// <summary>Determines the progress of an ij loop in a rectangle.</summary>
+    /// <param name="i"></param>
+    /// <param name="j"></param>
+    /// <param name="leftSide">X coordinate of the Left edge of the rectangle.</param>
+    /// <param name="rightSide">X coordinate of the Right edge of the rectnagle.</param>
+    /// <param name="topSide">Y coordinate of the Top Edge of the rectangle.</param>
+    /// <param name="bottomSide">Y coordinate of the Bottom Edge of the rectangle.</param>
+    protected static double RectangleProgress(int i, int j, int leftSide, int rightSide, int topSide, int bottomSide) {
+        return (i * Main.maxTilesY + j) / (double)(rightSide - leftSide + (bottomSide - topSide));
+    }
+
+    /// <summary>
+    /// Safely sets the progress bar.
+    /// </summary>
+    protected void SetProgress(GenerationProgress progress, double progressValue, double prevProgress = 0f, double wantedProgress = 1f) {
+        if (progress != null) {
+            progress.Value = (prevProgress + (wantedProgress - prevProgress) * Math.Clamp(progressValue, 0f, 1f)) / Weight;
+        }
+    }
+
+    protected static void AddStructure(int i, int j, int width, int height, int padding = 0) {
+        AddStructure(new Rectangle(i, j, width, height), padding);
+    }
+    protected static void AddStructure(Rectangle rectangle, int padding = 0) {
+        GenVars.structures.AddStructure(rectangle, padding);
+    }
+
+    protected static void AddProtectedStructure(int i, int j, int width, int height, int padding = 0) {
+        AddProtectedStructure(new Rectangle(i - width / 2, j - height / 2, width, height), padding);
+    }
+    protected static void AddProtectedStructure(Rectangle rectangle, int padding = 0) {
+        GenVars.structures.AddProtectedStructure(rectangle, padding);
     }
 }
