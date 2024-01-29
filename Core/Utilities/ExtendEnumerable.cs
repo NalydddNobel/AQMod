505fbbfe9b63b8f916639e069a9259691e290d3b
NalydddNobel <kainnobel6@gmail.com>
2024-01-28 22:48:55 -0800

    util docs and added extendsound

--- Core/Utilities/ExtendEnumerable.cs
+++ Core/Utilities/ExtendEnumerable.cs
@@ -5,6 +5,8 @@ using System.Linq;
 namespace Aequus.Core.Utilities;
 
 public static class ExtendEnumerable {
+    /// <summary>Forces an Enumeration.</summary>
+    /// <returns>Whether an instance of <paramref name="en2"/> is in <paramref name="en"/>.</returns>
     public static bool Match<T>(this IEnumerable<T> en, T en2) {
         return en.Any((t) => t.Equals(en2));
     }
