d2407c0cd691b16af95836812719312f67ad4fae
NalydddNobel <kainnobel6@gmail.com>
2024-02-04 23:51:48 -0800

    actually fixed everything wtf

--- Core/UI/UILayer.cs
+++ Core/UI/UILayer.cs
@@ -27,7 +27,9 @@ public abstract class UILayer : GameInterfaceLayer, ILoadable {
 
     public virtual void OnClearWorld() { }
     public virtual void OnPreUpdatePlayers() { }
-    public virtual bool OnUIUpdate(GameTime gameTime) => true;
+    public virtual bool OnUIUpdate(GameTime gameTime) {
+        return true;
+    }
 
     protected virtual void OnActivate() { }
     protected virtual void OnDeactivate() { }
