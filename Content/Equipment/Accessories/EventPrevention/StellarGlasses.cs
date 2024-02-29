42ceccec849fedf95330b584251c1970a7c4a06d
NalydddNobel <kainnobel6@gmail.com>
2024-02-28 19:14:33 -0800

    stellar glasses now glow when equipped

--- Content/Equipment/Accessories/EventPrevention/StellarGlasses.cs
+++ Content/Equipment/Accessories/EventPrevention/StellarGlasses.cs
@@ -1,13 +1,38 @@
-﻿using Aequus.Old.Content.Materials;
+﻿using Aequus.Common.Players.Drawing;
+using Aequus.Old.Content.Materials;
+using System;
+using Terraria.DataStructures;
+using Terraria.GameContent;
 
 namespace Aequus.Content.Equipment.Accessories.EventPrevention;
 
-[AutoloadEquip(EquipType.Face)]
 public class StellarGlasses : ModItem {
+    private class StellarGlassesEquipTexture : EquipTexture, IEquipTextureDraw {
+        public void Draw(ref PlayerDrawSet drawInfo, Vector2 position, Rectangle frame, Color color, float rotation, Vector2 origin, SpriteEffects effects, int shader) {
+            Texture2D texture = TextureAssets.AccFace[drawInfo.drawPlayer.face].Value;
+            color = Color.Lerp(color, Color.White with { A = color.A } * (1f - drawInfo.shadow), 0.8f);
+
+            drawInfo.Draw(texture, position, frame, color, rotation, origin, 1f, effects, shader);
+            float time = Main.GlobalTimeWrappedHourly * 0.5f % 2f;
+            if (time < 1f) {
+                float waveFunction = MathF.Sin(time * MathHelper.Pi);
+                Color glowColor = color with { A = 0 } * 0.2f * waveFunction;
+                for (int i = 0; i < 4; i++) {
+                    drawInfo.Draw(texture, position + (i * MathHelper.PiOver2 + time * 5f).ToRotationVector2() * 2f * waveFunction, frame, glowColor, rotation, origin, 1f, effects, shader);
+                }
+            }
+        }
+    }
+
+    public override void Load() {
+        Item.faceSlot = EquipLoader.AddEquipTexture(Mod, AequusTextures.StellarGlasses_Face.Path, EquipType.Face, this, null, new StellarGlassesEquipTexture());
+    }
+
     public override void SetDefaults() {
         Item.DefaultToAccessory();
         Item.rare = ItemRarityID.Cyan;
         Item.value = Item.sellPrice(gold: 2);
+        Item.faceSlot = EquipLoader.GetEquipSlot(Mod, Name, EquipType.Face);
     }
 
     public override void UpdateEquip(Player player) {
@@ -15,11 +40,7 @@ public class StellarGlasses : ModItem {
     }
 
     public override Color? GetAlpha(Color lightColor) {
-        return Color.Lerp(lightColor, Color.White, 0.5f);
-    }
-
-    public override void DrawArmorColor(Player drawPlayer, float shadow, ref Color color, ref int glowMask, ref Color glowMaskColor) {
-        color = Color.Lerp(color, Color.White, 0.5f);
+        return Color.Lerp(lightColor, Color.White, 0.8f);
     }
 
     public override void AddRecipes() {
--- Core/Utilities/ExtendPlayer.cs
+++ Core/Utilities/ExtendPlayer.cs
@@ -286,4 +286,22 @@ public static class ExtendPlayer {
         return PlayerDeathReason.ByCustomReason(Language.GetTextValue(key));
     }
     #endregion
+
+    #region Player Draw Set
+    public static void Draw(this ref PlayerDrawSet drawInfo, Texture2D texture, Vector2 position, Rectangle? sourceRect, Color color, float rotation, Vector2 origin, Vector2 scale, SpriteEffects effect, int shader, float inactiveLayerDepth = 0f) {
+        drawInfo.DrawDataCache.Add(new DrawData(texture, position, sourceRect, color, rotation, origin, scale, effect, inactiveLayerDepth) with { shader = shader });
+    }
+    public static void Draw(this ref PlayerDrawSet drawInfo, Texture2D texture, Vector2 position, Rectangle? sourceRect, Color color, float rotation, Vector2 origin, float scale, SpriteEffects effect, int shader, float inactiveLayerDepth = 0f) {
+        drawInfo.DrawDataCache.Add(new DrawData(texture, position, sourceRect, color, rotation, origin, scale, effect, inactiveLayerDepth) with { shader = shader });
+    }
+    public static void Draw(this ref PlayerDrawSet drawInfo, Texture2D texture, Vector2 position, Rectangle? sourceRect, Color color, float rotation, Vector2 origin, Vector2 scale, SpriteEffects effect, float inactiveLayerDepth = 0f) {
+        drawInfo.DrawDataCache.Add(new DrawData(texture, position, sourceRect, color, rotation, origin, scale, effect, inactiveLayerDepth));
+    }
+    public static void Draw(this ref PlayerDrawSet drawInfo, Texture2D texture, Vector2 position, Rectangle? sourceRect, Color color, float rotation, Vector2 origin, float scale, SpriteEffects effect, float inactiveLayerDepth = 0f) {
+        drawInfo.DrawDataCache.Add(new DrawData(texture, position, sourceRect, color, rotation, origin, scale, effect, inactiveLayerDepth));
+    }
+    public static void Draw(this ref PlayerDrawSet drawInfo, DrawData drawData) {
+        drawInfo.DrawDataCache.Add(drawData);
+    }
+    #endregion
 }
