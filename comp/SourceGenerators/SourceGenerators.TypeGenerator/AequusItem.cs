7a6dc68e2c74c9e9604fb4207030c9066f882d18
NalydddNobel <kainnobel6@gmail.com>
2024-06-10 02:44:30 -0700

    fix auto files

--- comp/SourceGenerators/SourceGenerators.TypeGenerator/AequusItem.cs
+++ comp/SourceGenerators/SourceGenerators.TypeGenerator/AequusItem.cs
@@ -6,4 +6,9 @@ using Aequus.Core.Structures;
 namespace Aequus;
 
 public partial class AequusItem {
+    
+    [CompilerGenerated]
+    private void UseItemInner(Item item, Player player, AequusPlayer aequusPlayer) {
+        Old.Content.Items.Accessories.HyperCrystal.HyperCrystal.OnUseItem(item, player, aequusPlayer);
+    }
 }
