c250460fbc2823d40764b96be705ea91e49f6eb8
NalydddNobel <kainnobel6@gmail.com>
2024-07-22 00:23:14 -0700

    i dont wanna finish this tonight

---
+++ Common/Structures/Target.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aequus.Common.Structures;
+
+public class Target {
+    private static readonly List<RenderTarget2D> _loadedRenderTargets = [];
+
+    public static RenderTarget2D NewRenderTarget(GraphicsDevice gd, int width, int height, RenderTargetUsage usage = RenderTargetUsage.DiscardContents) {
+        return NewRenderTarget(new RenderTarget2D(gd, width, height, mipMap: false, SurfaceFormat.Color, DepthFormat.None, preferredMultiSampleCount: 0, usage));
+    }
+
+    public static RenderTarget2D NewRenderTarget(RenderTarget2D target) {
+        if (!_loadedRenderTargets.Contains(target)) {
+            _loadedRenderTargets.Add(target);
+        }
+
+        return target;
+    }
+
+    public static bool CheckTargetCycle(ref RenderTarget2D target, int desiredWidth, int desiredHeight, GraphicsDevice device = null, RenderTargetUsage usage = RenderTargetUsage.DiscardContents) {
+        if (!BadRenderTarget(target, desiredWidth, desiredHeight)) {
+            return true;
+        }
+
+        device ??= Main.instance.GraphicsDevice;
+        try {
+            DiscardTarget(ref target);
+
+            if (Main.IsGraphicsDeviceAvailable) {
+                target = NewRenderTarget(device, desiredWidth, desiredHeight, RenderTargetUsage.PreserveContents);
+            }
+        }
+        catch (Exception ex) {
+            Aequus.Instance.Logger.Error(ex);
+        }
+        finally {
+        }
+        return false;
+    }
+
+    public static void DiscardTarget(ref RenderTarget2D target) {
+        if (target == null) {
+            return;
+        }
+
+        _loadedRenderTargets.Remove(target);
+        if (!target.IsDisposed) {
+            Main.QueueMainThreadAction(target.Dispose);
+        }
+
+        target = null;
+    }
+
+    public static bool BadRenderTarget(RenderTarget2D renderTarget2D) {
+        return renderTarget2D == null || renderTarget2D.IsDisposed || renderTarget2D.IsContentLost;
+    }
+    public static bool BadRenderTarget(RenderTarget2D renderTarget2D, int desiredWidth, int desiredHeight) {
+        return BadRenderTarget(renderTarget2D) || renderTarget2D.Width != desiredWidth || renderTarget2D.Height != desiredHeight;
+    }
+}
---
+++ Content/Tiles/ReflectiveBlock.cs
@@ -0,0 +1,86 @@
+﻿using Aequus.Common.Graphics;
+using Aequus.Common.Structures;
+using System;
+using System.Collections.Generic;
+
+namespace Aequus.Content.Tiles;
+
+// Unfinished.
+
+public class ReflectiveBlock {
+}
+
+public class ReflectiveEffect : RequestHandler<ReflectiveSurfaceRequest> {
+    public bool Ready { get; private set; }
+
+    private RenderTarget2D _target;
+
+    protected override void OnActivate() {
+        DrawLayers.Instance.PostUpdateScreenPosition += HandleRequestsOnPreDraw;
+        DrawLayers.Instance.PostDrawLiquids += DrawTargetOntoScreen;
+    }
+
+    protected override void OnDeactivate() {
+        Ready = false;
+        DrawLayers.Instance.PostUpdateScreenPosition -= HandleRequestsOnPreDraw;
+        DrawLayers.Instance.PostDrawLiquids -= DrawTargetOntoScreen;
+    }
+
+    private void HandleRequestsOnPreDraw(SpriteBatch sb) {
+        HandleRequests();
+        ClearQueue();
+    }
+
+    protected override bool HandleRequests(IEnumerable<ReflectiveSurfaceRequest> todo) {
+        if (Main.instance.tileTarget == null || Main.instance.tileTarget.IsDisposed || Main.instance.tileTarget.IsContentLost) {
+            return false;
+        }
+
+        SpriteBatch spriteBatch = Main.spriteBatch;
+        GraphicsDevice device = Main.instance.GraphicsDevice;
+
+        if (!Target.CheckTargetCycle(ref _target, Main.instance.tileTarget.Width, Main.instance.tileTarget.Height, device, RenderTargetUsage.PreserveContents)) {
+            return false;
+        }
+
+        RenderTargetBinding[] oldTargets = device.GetRenderTargets();
+
+        device.SetRenderTarget(_target);
+        device.Clear(Color.Transparent);
+        spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+        try {
+            DrawRequests(spriteBatch, todo);
+        }
+        catch (Exception ex) {
+            Mod.Logger.Error(ex);
+        }
+        finally {
+            spriteBatch.End();
+        }
+
+        device.SetRenderTargets(oldTargets);
+
+        return true;
+    }
+
+    private void DrawRequests(SpriteBatch spriteBatch, IEnumerable<ReflectiveSurfaceRequest> todo) {
+        Ready = false;
+        if (Main.drawToScreen || !Lighting.NotRetro || !Aequus.HQ) {
+            return;
+        }
+
+        foreach (ReflectiveSurfaceRequest request in todo) {
+
+        }
+
+        Ready = true;
+    }
+
+    private void DrawTargetOntoScreen(SpriteBatch spriteBatch) {
+        if (_target == null || !Ready) {
+            return;
+        }
+    }
+}
+
+public readonly record struct ReflectiveSurfaceRequest();
