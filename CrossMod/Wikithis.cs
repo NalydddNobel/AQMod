9d45d49c42837ee9cf4d287e50962baa54efd5f1
NalydddNobel <kainnobel6@gmail.com>
2024-08-04 02:10:02 -0700

    fix wiki this support

--- CrossMod/Wikithis.cs
+++ CrossMod/Wikithis.cs
@@ -1,13 +1,10 @@
 ﻿using Aequus.Common.CrossMod;
-using Terraria;
-using Terraria.ModLoader;
 
-namespace Aequus.CrossMod {
-    internal class Wikithis : ModSupport<Wikithis> {
-        public override void SafeLoad(Mod mod) {
-            if (mod != null && !Main.dedServ) {
-                mod.Call("AddModURL", Mod, "terrariamods.wiki.gg$Aequus");
-            }
+namespace Aequus.CrossMod;
+internal class Wikithis : ModSupport<Wikithis> {
+    public override void SafeLoad(Mod mod) {
+        if (mod != null && !Main.dedServ) {
+            mod.Call("AddModURL", this, "https://examplemod.wiki.gg/wiki/{}");
         }
     }
 }
