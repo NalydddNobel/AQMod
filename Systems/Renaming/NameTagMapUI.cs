13a6de4353aed205e6e03f075982acc11cbf30fd
NalydddNobel <kainnobel6@gmail.com>
2024-08-04 23:01:48 -0700

    add map blip system

---
+++ Systems/Renaming/NameTagMapUI.cs
@@ -0,0 +1,14 @@
+﻿using Terraria.Map;
+using Terraria.UI;
+
+namespace Aequus.Systems.Renaming;
+
+public class NameTagMapUI : ModMapLayer {
+    public override void Draw(ref MapOverlayDrawContext context, ref string text) {
+        foreach (var npc in RenamingSystem.RenamedNPCs) {
+            if (context.Draw(AequusTextures.NameTagBlip, new Vector2(npc.Value.tileX + 0.5f, npc.Value.tileY + 0.5f), Alignment.Center).IsMouseOver) {
+                text = $"{npc.Value.customName} ({Lang.GetNPCNameValue(npc.Value.type)})";
+            }
+        }
+    }
+}
