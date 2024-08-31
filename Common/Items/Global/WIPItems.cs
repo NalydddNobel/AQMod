2484730bc3e262629069766812aab7986b9597ff
NalydddNobel <kainnobel6@gmail.com>
2024-08-31 05:29:25 -0700

    wip items no longer get tooltip in debug

--- Common/Items/Global/WIPItems.cs
+++ Common/Items/Global/WIPItems.cs
@@ -1,20 +1,23 @@
-﻿using Microsoft.Xna.Framework;
-using ReLogic.Utilities;
+﻿using ReLogic.Utilities;
 using System.Collections.Generic;
-using Terraria;
-using Terraria.ModLoader;
 
-namespace Aequus.Common.Items.Global {
-    public class WIPItems : GlobalItem {
-        public override bool AppliesToEntity(Item item, bool lateInstantiation) {
-            return item.ModItem != null && item.ModItem.GetType().GetAttribute<WorkInProgressAttribute>() != null;
-        }
+namespace Aequus.Common.Items.Global;
 
-        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
-            tooltips.Insert(
-                tooltips.GetIndex("OneDropLogo"),
-                new TooltipLine(Mod, "WorkInProgress", TextHelper.GetTextValue("Items.WorkInProgress")) { OverrideColor = Color.LightCoral * 1.33f, }
-            );
-        }
+public class WIPItems : GlobalItem {
+#if DEBUG
+    public override bool IsLoadingEnabled(Mod mod) {
+        return false;
+    }
+#endif
+
+    public override bool AppliesToEntity(Item item, bool lateInstantiation) {
+        return item.ModItem != null && item.ModItem.GetType().GetAttribute<WorkInProgressAttribute>() != null;
+    }
+
+    public override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
+        tooltips.Insert(
+            tooltips.GetIndex("OneDropLogo"),
+            new TooltipLine(Mod, "WorkInProgress", TextHelper.GetTextValue("Items.WorkInProgress")) { OverrideColor = Color.LightCoral * 1.33f, }
+        );
     }
 }
