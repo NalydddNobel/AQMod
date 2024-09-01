c86d00e2ccbcec3c5810ffa20a4da0ccfbf6a4e4
NalydddNobel <kainnobel6@gmail.com>
2024-09-01 14:17:10 -0700

    final wyvernwood staff tweaks

--- Content/Items/Weapons/Magic/MeadowStaff/MeadowStaff.cs
+++ Content/Items/Weapons/Magic/MeadowStaff/MeadowStaff.cs
@@ -15,7 +15,7 @@ public class MeadowStaff : ModItem, IHaveCrossedOutIndicator {
     }
 
     public override void SetDefaults() {
-        Item.mana = 0;
+        Item.mana = 10;
         Item.UseSound = SoundID.Item43;
         Item.useStyle = ItemUseStyleID.Shoot;
         Item.damage = 18;
@@ -33,31 +33,29 @@ public class MeadowStaff : ModItem, IHaveCrossedOutIndicator {
     }
 
     public override bool CanUseItem(Player player) {
+        return true;
         return !player.manaSick;
     }
 
-    public override bool AltFunctionUse(Player player) {
-        return player.statMana > 1;
-    }
+    //public override bool AltFunctionUse(Player player) {
+    //    return player.statMana > 1;
+    //}
 
-    public override bool? UseItem(Player player) {
+    //public override bool? UseItem(Player player) {
 
-        return base.UseItem(player);
-    }
+    //    return base.UseItem(player);
+    //}
 
     public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
-        int mana = player.statMana;
-        if (player.altFunctionUse == 2 && player.CheckMana(mana, pay: true)) {
-            Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<MeadowStaffLaser>(), damage, knockback, player.whoAmI, ai0: mana);
-            return false;
-        }
+        //int mana = player.statMana;
+        //if (player.altFunctionUse == 2 && player.CheckMana(mana, pay: true)) {
+        //    Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<MeadowStaffLaser>(), damage, knockback, player.whoAmI, ai0: mana);
+        //    return false;
+        //}
         return true;
     }
 
     public override void AddRecipes() {
-#if !DEBUG
-        return;
-#endif
         CreateRecipe()
             .AddIngredient(Instance<Biomes.Meadows.Tiles.MeadowWood>().Item.Type, 14)
             .AddTile(TileID.WorkBenches)
@@ -71,6 +69,12 @@ public class MeadowStaff : ModItem, IHaveCrossedOutIndicator {
 
 public class MeadowStaffProj : ModProjectile {
     public override LocalizedText DisplayName => Instance<MeadowStaff>().DisplayName;
+
+    public override void SetStaticDefaults() {
+        ProjectileID.Sets.TrailCacheLength[Type] = 28;
+        ProjectileID.Sets.TrailingMode[Type] = 2;
+    }
+
     public override void SetDefaults() {
         Projectile.width = 10;
         Projectile.height = 10;
@@ -78,8 +82,7 @@ public class MeadowStaffProj : ModProjectile {
         Projectile.DamageType = DamageClass.Magic;
         Projectile.extraUpdates = 1;
         Projectile.alpha = 255;
-        ProjectileID.Sets.TrailCacheLength[Type] = 35;
-        ProjectileID.Sets.TrailingMode[Type] = 2;
+        Projectile.penetrate = -1;
     }
 
     public override void AI() {
@@ -88,21 +91,23 @@ public class MeadowStaffProj : ModProjectile {
         player.manaRegenCount = 0;
         player.manaRegenDelay = player.maxRegenDelay;
 
-        Projectile.velocity *= 0.985f;
+        Projectile.velocity *= 0.99f;
 
         if (Projectile.velocity != Vector2.Zero) {
             Projectile.rotation = (Projectile.position - Projectile.oldPos[1]).ToRotation();
+
+            if (Projectile.ai[0] == 0f) {
+                Projectile.ai[0] = Main.rand.NextFloat(0.25f, 0.3f);
+                Projectile.netUpdate = true;
+            }
+            else {
+                float magnitude = 12f * Projectile.ai[0] * Projectile.Opacity;
+                Vector2 lastOffset = Projectile.oldVelocity.RotatedBy(MathHelper.PiOver2) * MathF.Sin((Projectile.localAI[0] - 1) * Projectile.ai[0]);
+                Vector2 offset = Projectile.velocity.RotatedBy(MathHelper.PiOver2) * MathF.Sin(Projectile.localAI[0] * Projectile.ai[0]);
+                Projectile.Center += (offset - lastOffset) * magnitude;
+            }
         }
 
-        if (Projectile.ai[0] == 0f) {
-            Projectile.ai[0] = Main.rand.NextFloat(0.2f, 0.4f);
-        }
-        else {
-            float magnitude = 5f * Projectile.Opacity;
-            Vector2 lastOffset = Projectile.oldVelocity.RotatedBy(MathHelper.PiOver2) * MathF.Sin((Projectile.localAI[0] - 1) * Projectile.ai[0]);
-            Vector2 offset = Projectile.velocity.RotatedBy(MathHelper.PiOver2) * MathF.Sin(Projectile.localAI[0] * Projectile.ai[0]);
-            Projectile.Center += (offset - lastOffset) * magnitude;
-        }
         Projectile.localAI[0]++;
 
         float speed = Projectile.velocity.Length();
@@ -120,21 +125,33 @@ public class MeadowStaffProj : ModProjectile {
         }
 
         if (speed > 1.6f && Projectile.alpha < 240 && (Projectile.numUpdates == -1 || Main.rand.NextBool(10))) {
-            Dust d = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<MonoDust>(), SpeedX: Projectile.velocity.X, SpeedY: Projectile.velocity.Y, newColor: Color.SkyBlue with { A = 100 });
+            Color color = new Color(158, 233, 223, 100);
+            Dust d = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<MonoDust>(), SpeedX: Projectile.velocity.X, SpeedY: Projectile.velocity.Y, newColor: color);
             //d.velocity *= 0.5f;
         }
 
         base.AI();
     }
+
+    public override bool OnTileCollide(Vector2 oldVelocity) {
+        Projectile.tileCollide = false;
+        Projectile.velocity *= 0f;
+        Projectile.netUpdate = true;
+        return false;
+    }
+
     public override bool PreDraw(ref Color lightColor) {
         Texture2D texture = TextureAssets.Projectile[Type].Value;
         Rectangle frame = texture.Bounds;
         Vector2 origin = new Vector2(frame.Width - 4, frame.Height / 2f);
         Vector2 drawOffset = Projectile.Size / 2f - Main.screenPosition;
         Vector2 scale = new Vector2(Math.Max(Projectile.velocity.Length() * 0.5f, 1f), 1f) * Projectile.scale;
-        Color color = Color.White with { A = 128 } * Projectile.Opacity * 0.5f;
+        Color color = Color.White with { A = 200 } * Projectile.Opacity * 0.5f;
         float trailLength = ProjectileID.Sets.TrailCacheLength[Type];
         for (int i = 0; i < trailLength; i++) {
+            if (i < trailLength - 2 && Projectile.oldPos[i] == Projectile.oldPos[i + 1]) {
+                continue;
+            }
             float t = 1f - i / trailLength;
             Main.EntitySpriteDraw(texture, Projectile.oldPos[i] + drawOffset, frame, color * t, Projectile.oldRot[i], origin, scale, SpriteEffects.None);
         }
