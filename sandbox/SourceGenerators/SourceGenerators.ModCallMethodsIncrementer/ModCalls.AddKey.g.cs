b184c7baa1575921d3578709e0d0e8f2f1f19473
NalydddNobel <kainnobel6@gmail.com>
2024-10-08 02:30:56 -0700

    add mod call for keychain

---
+++ sandbox/SourceGenerators/SourceGenerators.ModCallMethodsIncrementer/ModCalls.AddKey.g.cs
@@ -0,0 +1,28 @@
+﻿namespace Aequus;
+
+public partial class ModCallHandler {
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    static void AddKey(object[] args) {
+        if (args[1] is not int itemId) {
+            LogError($"Mod Call Parameter index 1 (\"itemId\") did not match Type \"int\".");
+            return;
+        }
+
+        if (args[2] is not string texturePath) {
+            LogError($"Mod Call Parameter index 2 (\"texturePath\") did not match Type \"string\".");
+            return;
+        }
+
+        bool nonConsumable = default;
+        if (args.Length > 3) {
+            if (args[3] is bool optionalValue) {
+                nonConsumable = optionalValue;
+            }
+            else {
+                LogError($"Optional Mod Call Parameter index 3 (\"nonConsumable\") did not match Type \"bool\".");
+            }
+        }
+
+        global::Aequus.Content.Systems.Keys.Keychains.KeychainDatabase.AddKeychainInfo(itemId, texturePath, nonConsumable);
+    }
+}
